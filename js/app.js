b3b1b541e83818c2a0c73b8e0fa9ba7ed3a41e2a
Manu Vairagi <75036590+ManuVairagi1@users.noreply.github.com>
2021-03-31 11:57:31 +0530

    done

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const eBrousher = document.querySelector("e__brousher");
+const eBrousherButton = document.querySelector("button__ebrousher");
+
+eBrousherButton.addEventListener("click", function () {
+  eBrousher.classList.add("display");
+});
